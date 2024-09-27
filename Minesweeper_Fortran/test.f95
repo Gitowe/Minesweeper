d867ca48ab92a316b5e7deaf91b7d435037c47cf
Gitowe <oweglass364@gmail.com>
2024-09-27 02:03:57 -0500

    Adding Fortran and GoLang test files

---
+++ Minesweeper_Fortran/test.f95
@@ -0,0 +1,8 @@
+!! temp file for hello world in Fortran
+
+program test
+    implicit none
+    
+    print *, "Hello, World!"
+
+end program test
---
+++ Minesweeper_GO/test.go
@@ -0,0 +1,1 @@
+//Test file for minesweeper game in go language :D
